7e7a552055d5980d37edd0a48f34d6407e9f87e0
Sony Theakanath <stheakanath@gmail.com>
2012-08-11 06:31:40 -0700

    Initial Commit

---
+++ Final Exam Calculator/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Final Exam Calculator
+//
+//  Created by Sony Theakanath on 8/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Final Exam Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Final Exam Calculator
+//
+//  Created by Sony Theakanath on 8/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
